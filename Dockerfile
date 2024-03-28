f7b5c2c13bdc65e2639cc34d60994ffab592b177
Oto Brglez <otobrglez@gmail.com>
2024-03-28 10:44:02 +0100

    Cleanup and docker build.

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+# read: https://github.com/tikv/grpc-rs/issues/477#issuecomment-1354094205
+
+ARG RUST_VERSION=1.75-buster
+
+FROM rust:${RUST_VERSION} as builder
+
+WORKDIR /app
+
+ENV RUST_BACKTRACE=full
+ENV CGO_ENABLED=1
+ENV APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update -yyq && apt-get install -yyq \
+  build-essential lsb-release wget software-properties-common gnupg g++ clang \
+  lld protobuf-compiler pkg-config libclang-dev \
+  llvm-dev libssl-dev libclang-dev curl bash cmake golang-go 
+
+# Go stuff
+COPY ./libprom2json libprom2json
+COPY ./go.mod go.mod
+COPY ./go.sum go.sum
+
+
+# Rust stuff
+COPY ./src src
+COPY ./build.rs build.rs
+COPY Cargo.toml Cargo.toml
+COPY Cargo.lock Cargo.lock
+
+RUN go build \
+  -v \
+  --ldflags '-linkmode external -extldflags=-static' \
+  -o libprom2json/prom2json.a \
+  -buildmode=c-archive \
+  libprom2json/main.go
+
+RUN cargo build --locked --release && cargo install --path .
+
+FROM debian:buster-slim as runtime
+
+RUN apt-get update -yyq && \
+  apt-get install -yyq curl bash && \
+  rm -rf /var/lib/apt/lists/*
+
+COPY --from=builder /app/ella /usr/local/bin/ella
+CMD ["ella"]
--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 use std::path::PathBuf;
 use std::process::Command;
-use bindgen::CargoCallbacks;
 
 fn main() {
     let libdir_path = PathBuf::from("libprom2json")
@@ -10,6 +9,7 @@ fn main() {
     let mut go_build = Command::new("go");
     go_build
         .arg("build")
+        .arg("-v")
         .arg("-buildmode=c-archive")
         .arg("-o")
         .arg(libdir_path.join("libprom2json.a"))
@@ -17,13 +17,13 @@ fn main() {
 
     go_build.status().expect("Go build failed");
 
-    println!("cargo:rerun-if-changed={}", "libprom2json/main.go");
+    println!("cargo:rerun-if-changed={}", "libprom2json/libprom2json.h");
     println!("cargo:rustc-link-search=native={}", libdir_path.to_str().unwrap());
     println!("cargo:rustc-link-lib=static=prom2json");
 
     let bindings = bindgen::Builder::default()
         .header("libprom2json/libprom2json.h")
-        .parse_callbacks(Box::new(CargoCallbacks::new()))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
@@ -31,186 +31,5 @@ fn main() {
     bindings
         .write_to_file(out_path)
         .expect("Couldn't write bindings!");
-
 }
 
-/*
-fn main() {
-    let libdir_path = PathBuf::from("libprom2json")
-        // Canonicalize the path as `rustc-link-search` requires an absolute
-        // path.
-        .canonicalize()
-        .expect("cannot canonicalize path");
-
-    let headers_path = libdir_path.join("prom2json.h");
-    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
-
-    // This is the path to the static library file.
-    let obj_path = libdir_path.join("prom2json.o");
-    let lib_path = libdir_path.join("prom2json.a");
-
-    println!("cargo:rustc-link-lib=prom2json");
-
-    /*
-    if !std::process::Command::new("clang")
-        .arg("-c")
-        .arg("-o")
-        .arg(&obj_path)
-        .arg(libdir_path.join("hello.c"))
-        .output()
-        .expect("could not spawn `clang`")
-        .status
-        .success()
-    {
-        // Panic if the command was not successful.
-        panic!("could not compile object file");
-    }
-
-    if !std::process::Command::new("ar")
-        .arg("rcs")
-        .arg(lib_path)
-        .arg(obj_path)
-        .output()
-        .expect("could not spawn `ar`")
-        .status
-        .success()
-    {
-        // Panic if the command was not successful.
-        panic!("could not emit library file");
-    }
-    */
-
-    let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
-        .header(headers_path_str)
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
-        .parse_callbacks(Box::new(CargoCallbacks::new()))
-        // Finish the builder and generate the bindings.
-        .generate()
-        // Unwrap the Result and panic on failure.
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from("libprom2json").join("bindings.rs");
-    bindings
-        .write_to_file(out_path)
-        .expect("Couldn't write bindings!");
-}
-*/
-/*
-fn main() {
-    let out_path = PathBuf::from("./libprom2json");
-
-    let mut go_build = Command::new("go");
-    go_build
-        .arg("build")
-        .arg("-buildmode=c-archive")
-        .arg("-o")
-        .arg(out_path.join("prom2json.a"))
-        .arg("./libprom2json/main.go");
-
-    go_build.status().expect("Go build failed");
-
-    let bindings = bindgen::Builder::default()
-        .header(out_path.join("prom2json.h").to_str().unwrap())
-        .parse_callbacks(Box::new(CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-
-    println!("cargo:rerun-if-changed=libprom2json/main.go");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        out_path.to_str().unwrap()
-    );
-
-    println!("cargo:rustc-link-search={}", out_path.to_str().unwrap());
-    println!("cargo:rustc-link-lib=static={}", "prom2json");
-    // println!("cargo:rustc-link-lib=static={}", "prom2json");
-
-
-}
-*/
-
-/*
-fn main() {
-    // println!("cargo:rustc-link-search=./");
-    //println!("cargo:rustc-link-lib=static={}", "libprom2json/libprom2json.a");
-    //println!("cargo:rustc-link-lib=libprom2json");
-    // println!("cargo:rustc-link-search=./libprom2json");
-    // println!("cargo:rustc-link-lib=prom2json");
-    //println!("cargo:rustc-link-search=native={}", "./libprom2json");
-    //println!("cargo:rustc-link-lib=static=prom2json"); // the name of the library
-    //println!("cargo:rustc-link-search=native={}", "./libprom2json");
-    //println!("cargo:rustc-link-lib=static={}", "libprom2json");
-
-    let libdir_path = PathBuf::from("libprom2json")
-        .canonicalize()
-        .expect("cannot canonicalize path");
-
-    let headers_path = libdir_path.join("prom2json.h");
-    let headers_path_str = headers_path.to_str().expect("Path is not a valid string");
-    let obj_path = libdir_path.join("prom2json.o");
-    // let lib_path = libdir_path.join("prom2json.a");
-
-    // libdir_path.to_str().unwrap()
-    // println!("cargo:rustc-link-search=native={}", build_dir);
-
-
-   // println!("cargo:rustc-link-arg=-mmacosx-version-min=12.0");
-
-    println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
-    // println!("cargo:rustc-cdylib-link-arg=-Wl,-undefined,dynamic_lookup,--unresolved-symbols=ignore-in-object-files");
-
-    // println!("cargo:rustc-cdylib-link-arg=-Wl");
-    // println!("cargo:rustc-cdylib-link-arg=--allow-multiple-definition");
-    // println!("cargo:rustc-cdylib-link-arg=-undefined");
-    // println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
-
-    println!("cargo:rustc-flags=-l framework=CoreFoundation -l framework=Security -l framework=SystemConfiguration");
-
-
-    /*
-    println!("cargo:rustc-link-arg=-Wl");
-    println!("cargo:rustc-link-arg=-undefined");
-    println!("cargo:rustc-link-arg=dynamic_lookup");
-    */
-
-    // println!("cargo:rustc-link-lib=prom2json");
-
-    //println!("cargo:rustc-cdylib-link-arg=-undefined");
-    //println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
-
-    // println!("cargo:rustc-cdylib-link-arg=-Wl,-undefined,dynamic_lookup");
-
-    //#[cfg(target_os = "macos")]
-    //{
-
-    // println!("cargo:rustc-cdylib-link-arg=-undefined");
-    // println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
-    // println!("cargo:rustc-cdylib-link-arg=-Wl,-undefined,dynamic_lookup");
-
-    // println!("cargo:rustc-cdylib-link-arg=-Wl,--no-as-needed,-ldl,--as-needed");
-
-    // println!("cargo:rustc-link-lib=static=c++");
-    // println!("cargo:rustc-cdylib-link-arg=-Wl,--allow-multiple-definition");
-
-
-
-
-    let bindings = bindgen::Builder::default()
-        .header(headers_path_str)
-        .parse_callbacks(Box::new(CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings");
-
-    let out_path = PathBuf::from("libprom2json");
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
-}
-*/
